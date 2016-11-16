d0ef8bf26935b13aebb5fe83d56922c167f82f6b
Benjamin Rannow <b.rannow@familie-redlich.de>
2016-11-16 16:32:46 +0100

    create mapping for course to bison

--- Classes/Component/PreProcessor/PerformanceToBisonArray.php
+++ Classes/Component/PreProcessor/PerformanceToBisonArray.php
@@ -19,10 +19,16 @@ namespace CPSIT\CourseBisonExport\Component\PreProcessor;
  *  GNU General Public License for more details.
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use CPSIT\IhkofEvents\Domain\Model\Contact;
+use CPSIT\T3eventsCourse\Domain\Model\Certificate;
 use CPSIT\T3importExport\Component\PreProcessor\AbstractPreProcessor;
 use CPSIT\T3importExport\Component\PreProcessor\PreProcessorInterface;
+use DWenzel\T3events\Domain\Model\Audience;
+use DWenzel\T3events\Domain\Model\Genre;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
+use TYPO3\CMS\Extbase\Persistence\Generic\LazyObjectStorage;
 use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 
 /**
@@ -36,6 +42,8 @@ class PerformanceToBisonArray
     extends AbstractPreProcessor
     implements PreProcessorInterface
 {
+
+    const DATETIME_FORMAT = 'Y-m-d H:i:s';
 
     /**
      * Tells whether the configuration is valid
@@ -80,7 +88,124 @@ class PerformanceToBisonArray
     {
         $performanceArray = [];
 
+        $performanceArray['id'] = $this->getEntityValueFromPath($performance, 'uid', 0);
+        $performanceArray['seminarprovider'] = $this->getConfigurationValue($configuration, 'seminarprovider', '');
+
+        // what does it mean?
+        $performanceArray['contingent'] = '';
+
+        // what does it mean? interpreted as startDate
+        $performanceArray['timeJournal'] = '';
+
+        // ENUM (no,yes) - veröffentlichen
+        $performanceArray['bPublished'] = 'no';
+        // ENUM (no,yes)
+        $performanceArray['bPlanned'] = 'no';
+        // ENUM (no,yes)
+        $performanceArray['bChecked'] = 'no';
+        // ENUM (no,yes) - Zulassung nach SGB III §85 für Bildungsgutscheine?
+        $performanceArray['bVoucher'] = 'no';
+        // ENUM (no,yes) - Nach Veröffentlichung stehen lassen
+        $performanceArray['bKeep'] = 'no';
+
+        // Kursname - VARCHAR (255)
+        $performanceArray['name'] = substr($this->getEntityValueFromPath($performance, 'event.headline', ''), 0 ,255);
+
+        $performanceArray['aim'] = $this->getEntityValueFromPath($performance, 'event.goals', '');
+        // Inhaltsbeschreibung des Kurses
+        $performanceArray['description'] = $this->getEntityValueFromPath($performance, 'event.description', '');
+
+        // duration - as varchar (100)
+        $performanceArray['duration'] = substr($this->getEntityValueFromPath($performance, 'duration', ''), 0 ,100);
+
+        $now = new \DateTime();
+        $startDate = $this->getEntityValueFromPath($performance, 'date', $now);
+        $endDate = $this->getEntityValueFromPath($performance, 'endDate', $now);
+
+        $performanceArray['timeDescription'] = $this->getEntityValueFromPath($performance, 'classTime', '');
+        $performanceArray['timeStart'] = $startDate->format(static::DATETIME_FORMAT);
+        $performanceArray['timeEnd'] = $endDate->format(static::DATETIME_FORMAT);
+
+        $performanceArray['participantMax'] = $this->getEntityValueFromPath($performance, 'places', 0);
+
+        $performanceArray['price'] = number_format($this->getEntityValueFromPath($performance, 'price', 0.0), 2, ',', '.'). '€';
+
+        // what does it mean?
+        $performanceArray['providerNumber'] = '';
+
+        $performanceArray['placeName'] = $this->getEntityValueFromPath($performance, 'eventLocation.name', '');
+        $performanceArray['placeAdd'] = $this->getEntityValueFromPath($performance, 'eventLocation.details', '');
+        $performanceArray['placeStreet'] = $this->getEntityValueFromPath($performance, 'eventLocation.address', '');
+        $performanceArray['placePostoffice'] = $this->getEntityValueFromPath($performance, 'eventLocation.zip', '');
+        $performanceArray['placeCity'] = $this->getEntityValueFromPath($performance, 'eventLocation.place', '');
+
+        $certs = $this->getEntityValueFromPath($performance, 'event.certificate', []);
+        if (!empty($certs)) {
+            /** @var Certificate $cert */
+            $cert = $certs[0];
+            $performanceArray['completion'] = $cert->getTitle();
+        }
+
+        $contacts = $this->getEntityValueFromPath($performance, 'event.contactPersons', []);
+        if (!empty($contacts)) {
+            /** @var Contact $contact */
+            $contact = $contacts[0];
+            $performanceArray['contactName'] = $contact->getFullName();
+        }
+
+        $performanceArray['meta'] = $this->getEntityValueFromPath($performance, 'event.keywords', '');
+
+        $performanceArray['semGroups'] = $this->getSeminarGroups($performance, $configuration);
+
+        $performanceArray['semTopics'] = $this->getSeminarTopic($performance, $configuration);
+
         return $performanceArray;
+    }
+
+    /**
+     * @param \DWenzel\T3events\Domain\Model\Performance $performance
+     * @param $configuration
+     * @return array
+     */
+    protected function getSeminarGroups($performance, $configuration)
+    {
+        $seminarGroups = [];
+
+        $audiences = $this->getEntityValueFromPath($performance, 'event.audience', []);
+
+        /** @var Audience $audience */
+        foreach ($audiences as $audience) {
+            $seminarGroups[] = [
+                'semGroup' => [
+                    'text' => $audience->getTitle()
+                ]
+            ];
+        }
+
+        return $seminarGroups;
+    }
+
+    /**
+     * @param \DWenzel\T3events\Domain\Model\Performance $performance
+     * @param $configuration
+     * @return array
+     */
+    protected function getSeminarTopic($performance, $configuration)
+    {
+        $seminarTopics = [];
+
+        $genres = $this->getEntityValueFromPath($performance, 'event.genre', []);
+
+        /** @var Genre $genre */
+        foreach ($genres as $genre) {
+            $seminarTopics[] = [
+                'semTopic' => [
+                    'text' => $genre->getTitle()
+                ]
+            ];
+        }
+
+        return $seminarTopics;
     }
 
     /**
@@ -94,6 +219,12 @@ class PerformanceToBisonArray
         $value = ObjectAccess::getPropertyPath($entity, $path);
         if (empty($value)) {
             return $default;
+        }
+
+        if ($value instanceof LazyObjectStorage) {
+            return $value->toArray();
+        } elseif ($value instanceof LazyLoadingProxy) {
+            $value = $value->_loadRealInstance();
         }
 
         return $value;
